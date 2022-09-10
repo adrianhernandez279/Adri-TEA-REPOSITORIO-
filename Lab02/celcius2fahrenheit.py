972c4f52d4fd8a638875d81431c4ad6c1418efeb
Adrian Jose Hernandez Sanchez (24279) <adrian.hernandez@est.zamorano.edu>
2022-09-09 18:22:58 -0600

    Version 1 lab02

---
+++ Lab02/celcius2fahrenheit.py
@@ -0,0 +1,5 @@
+ # Tendencias e Innovacion en Tecnologia Agricola (TEA)
+tempCelcius = input("Cual es la temperatura en grados celcius?" )
+tempFahrenheit = ( int (tempCelcius) * int (1.8)) + 32
+
+print ( "temperatura en grados Fahrenheit = " + str (tempFahrenheit))
--- Lab02/celcius2farenheit.py
+++
@@ -1,7 +0,0 @@
- # Tendencias e Innovacion en Tecnologia Agricola (TEA)
-tempCelcius = input("Cual es la temperatura en grados celcius?" )
-tempFahrenheit = (tempCelcius * 1.8) + 32
-
-print ()
-
-#Ya casi
